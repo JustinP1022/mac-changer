ee5373a88c8ab820ff6173607da9c8a084ba4e74
JustinP1022 <justin@pixelsnprojects.com>
2018-08-28 19:04:06 -0400

    Worked on adding error messages and returning results

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -9,10 +10,10 @@ def get_arguments():
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = parser.parse_args()
     if not options.interface:
-        #code to handle error
+        # code to handle error
         parser.error("[-] Please specify an interface, use --help for more info")
     elif not options.new_mac:
-        #code to handle error
+        # code to handle error
         parser.error("[-] Please specify a new mac, use --help for more info")
     return options
 
@@ -23,4 +24,14 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 options = get_arguments()
-change_mac(options.interface, options.new_mac)
+# change_mac(options.interface, options.new_mac)
+
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
+print(ifconfig_result)
+
+mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+if mac_address_search_result:
+    print(mac_address_search_result.group(0))
+else:
+    print("[-] Could not read MAC address.")
