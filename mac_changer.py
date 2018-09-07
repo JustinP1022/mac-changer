3e416cfad2a25bc661da7cb4c0ebd64a3a996fd6
JustinP1022 <justin@pixelsnprojects.com>
2018-09-06 20:37:01 -0400

    Performed refactoring/housekeeping on code

--- mac_changer.py
+++ mac_changer.py
@@ -23,15 +23,16 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 # change_mac(options.interface, options.new_mac)
-
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
-
-mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
-if mac_address_search_result:
-    print(mac_address_search_result.group(0))
-else:
-    print("[-] Could not read MAC address.")
